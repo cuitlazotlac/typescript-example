7aabae770da12855c578b16d24c99e7f9cb82a66
Cuitlazotlac <cuitlazotlac@gmail.com>
2021-04-19 23:38:23 -0400

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Hello Typescript');
+console.log("Hello Typescript");
+// Implicit Types
+var helloWorld = "Hello World";
+// Explicit types
+var firstName = "John";
+var age = 30;
+var x = ["Hey", 5];
+// Enums
+var Continents;
+(function (Continents) {
+    Continents[Continents["North_America"] = 0] = "North_America";
+    Continents[Continents["South_America"] = 1] = "South_America";
+    Continents[Continents["Africa"] = 2] = "Africa";
+    Continents[Continents["Asia"] = 3] = "Asia";
+    Continents[Continents["Euripe"] = 4] = "Euripe";
+    Continents[Continents["Antartica"] = 5] = "Antartica";
+    Continents[Continents["Australia"] = 6] = "Australia";
+})(Continents || (Continents = {}));
+// usage
+var region = Continents.Africa;
--- index.ts
+++ index.ts
@@ -1,4 +1,51 @@
-console.log('Hello Typescript')
+console.log("Hello Typescript");
 
 // Implicit Types
 let helloWorld = "Hello World";
+
+// Explicit types
+let firstName: string = "John";
+let age: number = 30;
+
+// Tuple
+type stringAndNumber = [string, number];
+
+let x: stringAndNumber = ["Hey", 5];
+
+// Enums
+enum Continents {
+  North_America,
+  South_America,
+  Africa,
+  Asia,
+  Euripe,
+  Antartica,
+  Australia,
+}
+
+// usage
+var region = Continents.Africa;
+
+// Interface
+interface User {
+  name: string;
+  id: number;
+}
+
+const user: User = {
+  name: "John",
+  id: 0,
+};
+
+// Composing types -> Union
+type WindowStates = "open" | "close" | "minimized";
+type LockStates = "locked" | "unlocked";
+type OddNumberUnderTen = 1 | 3 | 5 | 7 | 9;
+
+const odd: OddNumberUnderTen = 5;
+
+const getLength = (param: string | string[]) => {
+    return param.length;
+}
+
+getLength('test');
